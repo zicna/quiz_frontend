87e6d983e102fcc809afba622a25d7414dacb0e9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-27 15:37:43 -0500

    add button for each category that will display quiz, previously set quiz.display='none'

--- src/category.js
+++ src/category.js
@@ -6,12 +6,25 @@ class Category{
     appendToDom(){
         const div = document.createElement("div")
         div.setAttribute("id", `category-${this.id}`)
+        const btn = document.createElement("button")
+        btn.innerHTML = `
+            See Quizzes
+        `
+        btn.addEventListener("click", this.handleClickBtnCategory)
         div.innerHTML = `
             <h5>${this.name}</h5>
         `
+        div.appendChild(btn)
         categoryContainer.appendChild(div)
     }
-    handleClickOnCategory(){
-        
+    handleClickBtnCategory(event){
+        if(event.target.innerText === "See Quizzes") {
+            event.target.innerText = "See Less"
+            event.target.nextElementSibling.style.display = "block"
+        } else if(event.target.innerText === "See Less"){
+            event.target.innerText = "See Quizzes"
+            event.target.nextElementSibling.style.display = "none"
+        }
+
     }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -7,10 +7,12 @@ class Quiz{
     appendQuizToDom(){
         const div = document.createElement("div")
         div.setAttribute("id", `quiz-${this.id}`)
+        div.setAttribute("style", "display:none")
         div.innerHTML = `
             <span style="display:none"> ${this.id}</span>
             <h4>${this.name}</h4>
             <span style="display:none">${this.category_id}</span>
+            <button>Start the Quiz</button>
         `
         const parentDiv = document.getElementById(`category-${this.category_id}`)
         parentDiv.appendChild(div)
