e619ea23f8af5999a8042b79a5c59f393219d89b
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 16:55:31 -0500

    get user based of form input

---
+++ src/user-service.js
@@ -0,0 +1,28 @@
+class UserService {
+  constructor(port) {
+    this.url = port + "/users";
+  }
+
+  getUser() {
+    //   debugger
+    const userInfo = {
+      user: {
+        username: username.value,
+        userEmail: userEmail.value,
+      },
+    };
+
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    },
+      body: JSON.stringify(userInfo),
+    };
+
+    fetch(this.url, configObject)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+}
