2a51351c5769ca5e824bda01a402a9389de928fe
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 16:19:30 -0500

    take down option old/new user from .html and .js

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const username = document.getElementById("username");
 const userEmail = document.getElementById("email");
 const userPassword = document.getElementById("password");
 
-let oldUser;
-function getRadioValue(arr) {
-  if (arr[0].checked) return true;
-  else if (arr[1].checked) return false;
-}
+// let oldUser;
+// function getRadioValue(arr) {
+//   if (arr[0].checked) return true;
+//   else if (arr[1].checked) return false;
+// }
 
 userForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const arrOfRadioBtns = Array.from(document.getElementsByName("old-user"));
-  oldUser = getRadioValue(arrOfRadioBtns);
+//   const arrOfRadioBtns = Array.from(document.getElementsByName("old-user"));
+//   oldUser = getRadioValue(arrOfRadioBtns);
 
   usersCall.getUser();
 }
