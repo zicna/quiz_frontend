1857f8cc9df783bf328ba89db50912a91e3873ad
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 17:22:06 -0500

    working on 'appendUserToDom' method

--- src/user.js
+++ src/user.js
@@ -1,9 +1,16 @@
-class User(){
+class User{
     constructor({id, username, email, admin}){
         this.id = id
         this.username = username
         this.email = email
         this.admin = admin
+    }
+    appendUserToDom(){
+        // debugger
+        userDisplayDIv.style.display = "block"
+        userDisplayUsername.innerText = this.username
+        userForm.style.display = "none"
+        debugger
     }
 
     
