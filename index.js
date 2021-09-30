211537b7689718553297c6f2a76256bf4fdb10f5
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 13:55:01 -0500

    adding new constants based of new element in index.html page

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const port = "http://localhost:3000/"
-const quizzesCall = new QuizService(port)
+const port = "http://localhost:3000"
+const usersCall = new UserService(port)
+
+// usersCall.getUser()
 
 //*HTML Elements constants
 const mainQuizDiv = document.getElementById("main-container")
+const startQuiz = document.getElementById("btn-start")
+const userForm = document.getElementById("user-form")
 
-quizzesCall.getQuizzes()
+
+
+
+
+userForm.addEventListener("submit", usersCall.getUser)
