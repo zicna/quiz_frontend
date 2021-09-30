87606c71c1ddf3ffe4df86a04ea00a71a6fd1dc1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 13:54:15 -0500

    create User.js file

---
+++ src/user.js
@@ -0,0 +1,10 @@
+class User(){
+    constructor({id, username, email, admin}){
+        this.id = id
+        this.username = username
+        this.email = email
+        this.admin = admin
+    }
+
+    
+}
