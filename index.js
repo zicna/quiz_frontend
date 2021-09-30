87d15ff8856e8d8da917e8b7a20c868642a32ff2
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-30 15:25:51 -0500

    set variables from user form

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const port = "http://localhost:3000"
-const usersCall = new UserService(port)
+const port = "http://localhost:3000";
+const usersCall = new UserService(port);
 
-// usersCall.getUser()
+// usersCall.getUser(port)
 
 //*HTML Elements constants
-const mainQuizDiv = document.getElementById("main-container")
-const startQuiz = document.getElementById("btn-start")
-const userForm = document.getElementById("user-form")
+const mainQuizDiv = document.getElementById("main-container");
+const startQuiz = document.getElementById("btn-start");
 
+const userForm = document.getElementById("user-form");
+const username = document.getElementById("username");
+const userEmail = document.getElementById("email");
+const userPassword = document.getElementById("password");
 
+let oldUser;
+function getRadioValue(arr) {
+  if (arr[0].checked) return true;
+  else if (arr[1].checked) return false;
+}
 
+userForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
 
-userForm.addEventListener("submit", usersCall.getUser)
+  const arrOfRadioBtns = Array.from(document.getElementsByName("old-user"));
+  oldUser = getRadioValue(arrOfRadioBtns);
+
+  usersCall.getUser();
+}
