bcdb129055a6b786ff6372f0d2396f97b00747f2
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-01 16:57:47 -0500

    redo constructor for User class

--- src/user.js
+++ src/user.js
@@ -1,16 +1,20 @@
 class User{
-    constructor({id, username, email, admin}){
+    constructor({id, username, email, admin, takes}){
         this.id = id
         this.username = username
         this.email = email
         this.admin = admin
+        this.takes = [
+            takes.map(take=>{
+                return new Take(take)
+            })
+        ]
     }
     appendUserToDom(){
-        // debugger
         userDisplayDIv.style.display = "block"
         userDisplayUsername.innerText = this.username
         userForm.style.display = "none"
-        debugger
+        
     }
 
     
