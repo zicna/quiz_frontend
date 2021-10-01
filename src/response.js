f0bb87fe0e727e8e0ac3c848ccb64f2cb78429c6
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-01 16:56:00 -0500

    add new file responsible to create responses for each take

---
+++ src/response.js
@@ -0,0 +1,9 @@
+class Response{
+    constructor({id, take_id, option_id, question_id, content}){
+        this.id = id
+        this.take_id = take_id
+        this.option_id = option_id
+        this.question_id = question_id
+        this.content = content
+    }
+}
