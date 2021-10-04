173bf42d163483c03e2ca4af65c8dda3f7755f7a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-04 11:18:49 -0500

    add Response attributes to correspond with apt json file

--- src/response.js
+++ src/response.js
@@ -1,9 +1,11 @@
 class Response{
-    constructor({id, take_id, option_id, question_id, content}){
+    constructor({id, take_id, option_id, question_id, content, is_true, url_explanation}){
         this.id = id
         this.take_id = take_id
         this.option_id = option_id
         this.question_id = question_id
         this.content = content
+        this.is_true = is_true
+        this.url_explanation = url_explanation
     }
 }
