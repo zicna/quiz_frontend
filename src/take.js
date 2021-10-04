195fa850ce5cf88efe2c81c6d0d1193ce95bc3d7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-04 12:15:49 -0500

    add new property of Take class 'totla_questions'(value is from api)

--- src/take.js
+++ src/take.js
@@ -1,8 +1,9 @@
 class Take{
-    constructor({id, user_id, quiz_id, responses}){
+    constructor({id, user_id, quiz_id, total_questions, responses}){
         this.id = id
         this.user_id = user_id
         this.quiz_id = quiz_id
+        this.total_questions = total_questions
         this.responses = [responses.map(response=>{
             return new Response(response)
         })]
