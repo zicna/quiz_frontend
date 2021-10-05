ddffbd8e3f62c312c73c7c49a9e3b193f091ae87
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-05 14:01:43 -0500

    delete comment out code

--- src/option.js
+++ src/option.js
@@ -8,12 +8,6 @@ class Option{
     appendOptionToDom(){
         const div = document.createElement("div")
         div.setAttribute("id", `option-${this.id}`)
-        // div.innerHTML = `
-        //     <span style="display:none"> ${this.id}</span>
-        //     <span><b>${this.content}</b></span>
-        //     <span style="display:none">${this.question_id}</span>
-        //     <span style="display:none">${this.is_correct}</span>
-        // `
         div.innerHTML = `
             <input type="radio" name="option-${this.question_id}">
             ${this.content}
