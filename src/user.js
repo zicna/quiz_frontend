8042edab642fe2e7773b3e0c42c7f5838d0a3143
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-07 14:44:30 -0500

    refactor 'appendUserToDom()' so it uses benefits of new Take properties (total_questions and num_correct_answers)

--- src/user.js
+++ src/user.js
@@ -12,30 +12,19 @@ class User {
     userDisplayDIv.style.display = "block";
     userDisplayUsername.innerText = `${this.username}`;
     userForm.style.display = "none";
-    // *move it to helper method?
-    const numOfQuestions = 7;
-    const correctAnswers = this.takes.map((take) => {
-      return take.responses.map((tr) => {
-        return tr.reduce((agg, curr) => {
-          if (curr.is_true) agg++;
-          return agg;
-        }, 0);
-      });
-    });
     
-    correctAnswers.map((el, index) => {
-      const row = document.createElement("tr");
-      // !we need to set data attribute for each row that will represent take id
+    // !we need to set data attribute for each row that will represent take id
       // !so we can target that take and delete it from
-      const percentCorrect = Math.round((el / numOfQuestions) * 100);
+    this.takes.map((el, index) => {
+      const row = document.createElement("tr");
+      const percentCorrect = Math.round((el.num_correct_answers / el.total_questions) * 100);
       row.innerHTML = `
         <td>${index + 1}</td>
-        <td>${el}</td>
-        <td>${numOfQuestions}</td>
+        <td>${el.num_correct_answers}</td>
+        <td>${el.total_questions}</td>
         <td>${percentCorrect} %</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
       `
-      
       userResultList.appendChild(row);
 
     });
