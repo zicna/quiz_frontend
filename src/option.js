d64d4e76c418609e1a1bc529d417978815cb500a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-10-08 12:30:35 -0500

    comment out methods that are not being used

--- src/option.js
+++ src/option.js
@@ -1,19 +1,20 @@
-class Option{
-    constructor({id, question_id, is_correct, content}){
-    this.id = id
-    this.question_id = question_id
-    this.is_correct = is_correct
-    this.content = content
-    }
-    appendOptionToDom(){
-        const div = document.createElement("div")
-        div.setAttribute("id", `option-${this.id}`)
-        div.innerHTML = `
-            <input type="radio" name="option-${this.question_id}">
-            ${this.content}
-            </input>
-        `
-        const parentDiv = document.getElementById(`question-${this.question_id}`)
-        parentDiv.appendChild(div)
-    }
+class Option {
+  constructor({ id, question_id, is_correct, content }) {
+    this.id = id;
+    this.question_id = question_id;
+    this.is_correct = is_correct;
+    this.content = content;
+  }
+  // appendOptionToDom(){
+  //     debugger
+  //     const div = document.createElement("div")
+  //     div.setAttribute("id", `option-${this.id}`)
+  //     div.innerHTML = `
+  //         <input type="radio" name="option-${this.question_id}">
+  //         ${this.content}
+  //         </input>
+  //     `
+  //     const parentDiv = document.getElementById(`question-${this.question_id}`)
+  //     parentDiv.appendChild(div)
+  // }
 }
--- src/question.js
+++ src/question.js
@@ -6,12 +6,13 @@ class Question {
     this.options = options.map((option) => new Option(option));
   }
 
-  appendQuestionToDom(){
-      const div = document.createElement("div")
-      div.setAttribute("id", `question-${this.id}`)
+  // appendQuestionToDom(){
+  //     const div = document.createElement("div")
+  //     div.setAttribute("id", `question-${this.id}`)
 
-      div.innerHTML = `<span>${this.content}</span>`
-      const parentDiv = document.getElementById(`q-q-${this.quiz_id}`)
-      parentDiv.appendChild(div)
-  }
+  //     div.innerHTML = `<span>${this.content}</span>`
+  //     const parentDiv = document.getElementById(`q-q-${this.quiz_id}`)
+  //     parentDiv.appendChild(div)
+  // }
 }
+//
